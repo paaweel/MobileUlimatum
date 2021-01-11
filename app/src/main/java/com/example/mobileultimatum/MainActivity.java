a6d13f19e1db4b2cd6be93c7301fb4b26a664389
Paweł Gakan <22177988+paaweel@users.noreply.github.com>
2021-01-11 20:18:48 +0100

    Add speech recognition (#2)
    Removed animate button
    Added text field
    Added listening button
    Added SpeechRecognizer to MainActivity

--- app/src/main/java/com/example/mobileultimatum/MainActivity.java
+++ app/src/main/java/com/example/mobileultimatum/MainActivity.java
@@ -1,11 +1,25 @@
 package com.example.mobileultimatum;
 
 
-import android.net.Uri;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.SkeletonNode;
@@ -14,33 +28,132 @@ import com.google.ar.sceneform.rendering.AnimationData;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private int i = 0;
+    private int i = 0; // todo: remove or at least rename
     private PepperSpeech pepperSpeech;
     private PepperMotion pepperMotion;
 
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+    private EditText editText;
+    private Button micButton;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // setup SpeechRecognizer
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            checkPermission();
+        }
+
+        editText = findViewById(R.id.speechTextView);
+        editText.setText("Hold mic to speak...");
+        micButton = findViewById(R.id.micBtn);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                editText.setText("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+                // TODO: create proper error handling
+                // refer to documentation for proper error message
+                editText.setText("Error..." + i);
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                // TODO: nicer button fo listening, color/icon change
+//                micButton.setImageResource(R.drawable.ic_launcher_background);
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                System.out.println(data);
+                editText.setText(data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        micButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            @SuppressLint("ClickableViewAccessibility")
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    // TODO: nicer button fo listening, color/icon change
+//                    micButton.setImageResource(R.drawable.ic_launcher_background);
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
+
+
+        // Setup ArFragment
         ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        assert arFragment != null;
         arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
             createModel(hitResult.createAnchor(), arFragment);
         }));
 
         pepperSpeech = new PepperSpeech(getApplicationContext());
         pepperMotion = new PepperMotion();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
+
+    private void checkPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
     }
 
     private void createModel(Anchor anchor, ArFragment arFragment) {
@@ -59,8 +172,10 @@ public class MainActivity extends AppCompatActivity {
 
                     arFragment.getArSceneView().getScene().addChild(anchorNode);
 
-                    Button button = findViewById(R.id.button);
-                    button.setOnClickListener(view -> animateModel(modelRenderable));
+                    // button has been removed
+                    // code kept for reference, delete once animations are properly handled
+//                    Button button = findViewById(R.id.animate_btn);
+//                    button.setOnClickListener(view -> animateModel(modelRenderable));
                 });
     }
 
